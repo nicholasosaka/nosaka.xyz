3b1e01b42cca885c8f581c7724f0f01944d525f2
Nikko Osaka <nikko.osaka99@gmail.com>
2020-03-28 17:19:23 -0400

    increase font size

--- components/PersonalInfo.js
+++ components/PersonalInfo.js
@@ -5,15 +5,15 @@ function PersonalInfo() {
     const titles = ["developer", "software engineer", "student", "coffee fanatic"];
     return (
         <span className="inline-block">
-            <p className="text-2xl font-thin -mb-">
+            <p className="text-3xl font-thin -mb-">
                 Hi, I'm
             </p>
 
-            <h1 className="text-4xl font-extrabold">
+            <h1 className="text-5xl font-extrabold">
                 Nicholas Osaka.
             </h1>
 
-            <div className="text-2xl flex flex-row font-thin">
+            <div className="text-3xl flex flex-row font-thin">
                 <span>I'm a</span>
                 <span className="ml-1">
                     <RestartingTypist style={{'color': '#FFFF00'}} words={titles} ></RestartingTypist>
