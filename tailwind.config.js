ac785c93d20d72d8c28fab6f6793552a5ebdbebb
Nicholas Osaka <nikko.osaka99@gmail.com>
2021-02-22 01:26:44 -0500

    Scaffold header content

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,23 @@
+const colors = require('tailwindcss/colors')
+
 module.exports = {
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'fogra': '#0A090C',
+        'jet': '#3B3A3D',
+        'ghost': '#F4F4F4',
+        'slate': '#6D8EA0',
+        'chestnut': '#BC4749',
+      },
+    },
   },
   variants: {
     extend: {},
   },
-  plugins: [],
+  plugins: [    
+    require('@tailwindcss/typography'),
+  ],
 }
