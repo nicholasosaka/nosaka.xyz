e6b5f4c1c521ddbb873df68154c44e8a9a215a5b
Nicholas Osaka <nicholas.osaka@gmail.com>
2021-08-03 11:01:11 -0400

    Fix width of experience bullet points

--- web/components/Resume.tsx
+++ web/components/Resume.tsx
@@ -70,7 +70,7 @@ const Work = (props: any) => {
         <p className="order-2 col-span-2 lg:col-span-1 lg:text-right tracking-tight">{props.location}</p>
         <p className="order-4 lg:order-3 col-span-4 lg:col-span-3 text-chestnut">{props.employer}</p>
         <p className="order-3 lg:order-4 col-span-3 lg:col-span-2 font-extralight tracking-tight text-right text-jet">{props.date}</p>
-        <ul className="order-5 col-span-4 pt-2 list-disc list-inside">
+        <ul className="order-5 col-span-5 pt-2 list-disc list-inside">
         {
             props.info.map(
                 function(info: any, idx: number) {
