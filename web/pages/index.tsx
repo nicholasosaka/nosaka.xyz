35324e165e1c5581de3980d300a2666790436471
Nicholas Osaka <nicholas.osaka@gmail.com>
2022-03-30 09:46:42 -0400

    Update currentPost logic

--- web/pages/index.tsx
+++ web/pages/index.tsx
@@ -5,6 +5,9 @@ import groq from 'groq'
 import sanity from '../lib/sanity'
 
 const Home = (props: any) => {
+  const currentPosts = props.posts.filter((p: { publishedAt: string }) => Date.parse(p.publishedAt) < Date.now() )
+  console.log(currentPosts)
+
   return (
     <div>
       <Head>
@@ -15,7 +18,7 @@ const Home = (props: any) => {
       <main> 
         <Header/>
         <div className="mt-8 md:mt-10 lg:mt-20 mx-auto w-5/6 md:w-9/12 lg:w-8/12">
-          <AllPosts {...props}/>
+          <AllPosts posts={currentPosts}/>
         </div>
       </main>
 
