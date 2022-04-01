e38b7cc852ace7e47a7d1b892b1fa2d25333fce3
Nicholas Osaka <nicholas.osaka@gmail.com>
2022-04-01 16:45:51 -0400

    Add paper + update postlayout schema

--- web/components/PostLayout.tsx
+++ web/components/PostLayout.tsx
@@ -3,6 +3,7 @@ import BlockContent from '@sanity/block-content-to-react'
 import Date from './Date'
 import PortableImage from './PortableText/PortableImage'
 import imageUrlBuilder from '@sanity/image-url'
+import Link from 'next/link'
 
 const builder = imageUrlBuilder(sanity)
 
@@ -15,12 +16,35 @@ const serializers = {
                 />
             )
         },
+        seperator: (props: any) => {
+            return <div className="border-t-2"></div>
+        },
+        Button: (props: any) => {
+            console.log(props)
+            return <Link href={props.node.uri} passHref>
+                        <button className="text-center font-sans duration-500 bg-transparent text-chestnut hover:bg-chestnut hover:text-white font-bold py-2 px-4 rounded inline-flex items-center border-2 border-chestnut">
+                            <svg className="fill-current w-4 h-4 mr-2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M13 8V2H7v6H2l8 8 8-8h-5zM0 18h20v2H0v-2z"/></svg>
+                            <span className="">{props.node.title}</span>
+                        </button>
+                    </Link>
+
+        }
     },
     marks: {
-        seperator: (props: any) => {
-            return (
-                <div className="border-t-2"></div>
-            )
+        highlight: (props: any) => {
+            return <span className='bg-[#fffb21]'>{props.children}</span>
+        },
+        underline: (props: any) => {
+            return <span className='underline'>{props.children}</span>
+        },
+        strikethrough: (props: any) => {
+            return <span className='line-through'>{props.children}</span>
+        },
+        superscript: (props: any) => {
+            return <sup>{props.children}</sup>
+        },
+        subscript: (props: any) => {
+            return <sub>{props.children}</sub>
         }
     }
 }
--- web/pages/index.tsx
+++ web/pages/index.tsx
@@ -5,7 +5,11 @@ import groq from 'groq'
 import sanity from '../lib/sanity'
 
 const Home = (props: any) => {
-  const currentPosts = props.posts.filter((p: { publishedAt: string }) => Date.parse(p.publishedAt) < Date.now() )
+
+  let posts = props.posts
+  if (process.env.NODE_ENV === 'production') {
+    posts = props.posts.filter((p: { publishedAt: string }) => Date.parse(p.publishedAt) < Date.now() )
+  }
 
   return (
     <div>
@@ -17,7 +21,7 @@ const Home = (props: any) => {
       <main className="bg-none lg:bg-[url('/svgs/blobs/blob-left.svg')] bg-bottom bg-no-repeat bg-cover lg:h-screen"> 
         <Header/>
         <div className="mt-8 md:mt-10 lg:mt-20 mx-auto w-5/6 md:w-9/12 lg:w-8/12">
-          <AllPosts posts={currentPosts}/>
+          <AllPosts posts={posts}/>
         </div>
       </main>
 
