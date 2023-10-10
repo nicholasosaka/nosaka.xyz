d2c474f264558c9c1e4b4ea7e275d2a5e765667e
Nicholas Osaka <nicholas.osaka@gmail.com>
2023-10-10 09:41:09 -0400

    Reorganize site layout

--- web/components/Header.tsx
+++ web/components/Header.tsx
@@ -4,15 +4,20 @@ const Header = () => {
     return (
     <>
         <div className="mx-auto md:w-8/12 w-4/5 sm:flex sm:flex-row items-center py-5 sm:py-10">
-            <h1 className="text-3xl lg:text-4xl font-extrabold uppercase text-left">
+            <h1 className="text-3xl font-extrabold uppercase text-left">
                 <Link href="/">
                 <a className="link-animation">Nicholas Osaka</a>
                 </Link>
             </h1>
             <ul className="list-none mx-auto mr-0 uppercase font-bold text-xl md:grid md:grid-cols-2">
-                <li className="inline mr-2">
-                    <Link href="/about">
-                        <a className="link-animation">About</a>
+                <li className="inline mr-5">
+                    <Link href="/writing">
+                        <a className="link-animation">writing</a>
+                    </Link>
+                </li>
+                <li className="inline">
+                    <Link href="/">
+                        <a className="link-animation">about</a>
                     </Link>
                 </li>
             </ul>
--- web/pages/index.tsx
+++ web/pages/index.tsx
@@ -1,43 +1,79 @@
-import Head from 'next/head'
+/* eslint-disable react/jsx-no-comment-textnodes */
 import Header from '@components/Header'
-import AllPosts from '@components/AllPosts'
+import Head from 'next/head'
 import groq from 'groq'
 import sanity from '../lib/sanity'
+import BlockContent from '@sanity/block-content-to-react'
+import serializers from '@lib/serializers'
+import IconLink from '@components/IconLink'
+import Link from 'next/link'
+import Image from 'next/image'
+import imageUrlBuilder from '@sanity/image-url'
 
-const Home = (props: any) => {
-  return (
-    <div>
-      <Head>
-        <title>Nicholas Osaka</title>
-        <link rel="icon" href="/favicon.ico" />
-        <meta name="description" content="Nicholas Osaka is a writer, worker in tech, and graduate student."/>
-      </Head>
 
-      <main className=""> 
-        <Header/>
-        <div className="mt-8 md:mt-10 lg:mt-20 mx-auto w-5/6 md:w-9/12 lg:w-8/12">
-          <AllPosts posts={props.posts}/>
+const About = (props: any) => {
+    const builder = imageUrlBuilder(sanity)
+    const imageUrl = builder.image(props.me.image).url()
+    return (
+        <div className="">
+            <Head>
+                <title>About Nicholas Osaka</title>
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+
+            <Header/>
+            <div className='mt-8 xl:mt-20 2xl:w-10/12 xl:w-11/12 md:w-8/12 mx-auto'>
+                <div className="flex flex-col xl:flex-row">
+                    <div className='px-10 py-10'>
+                        <h1 className="text-2xl uppercase font-extrabold text-chestnut">// about me</h1>
+                        <div className="xl:flex flex-row lg:w-9/12 justify-between my-3">
+                            <IconLink svg="/svgs/email.svg" alt="Email Icon" text="nosaka@charlotte.edu" link="mailto:nosaka@charlotte.edu"/>
+                            <IconLink svg="/svgs/github.svg" alt="GitHub Icon" text="nicholasosaka" link="https://github.com/nicholasosaka"/>
+                            <IconLink svg="/svgs/linkedin.svg" alt="LinkedIn Icon" text="nicholasosaka" link="https://linkedin.com/in/nicholasosaka/"/>
+                        </div>
+                        <div className="prose">
+                            <BlockContent
+                                blocks={props.me.bio}
+                                serializers={serializers}
+                                imageOptions={{ w: 320, h: 240, fit: 'max' }}
+                                {...sanity.config()}
+                            />
+                        </div>
+                        <h1 className="text-lg font-bold text-chestnut uppercase">// Academic CV</h1>
+                        <p className='prose'>You can find my academic CV here, as a PDF. I try to keep it as up-to-date as possible. For a look at my industry experience, please check out my LinkedIn.</p>
+                        <div className="flex md:flex-row flex-col gap-y-8 gap-x-16 my-8 w-full">
+                            <Link href="/cv.pdf" passHref>
+                                <button className="transition duration-500 bg-transparent text-chestnut hover:bg-chestnut hover:text-white font-bold py-2 px-28 rounded inline-flex items-center border-2 border-chestnut mx-auto">
+                                    <span className="mx-auto">View my CV</span>
+                                </button>
+                            </Link>
+                        </div>
+                    </div>
+                    <div className='order-first mx-auto pb-5 px-10'>
+                        <Image
+                            className="rounded-2xl"
+                            src="/headshot.jpg"
+                            width={1418/3}
+                            height={1772/3}
+                            alt="Picture of Nicholas Osaka"
+                            />
+                    </div>
+                </div>
+            </div>
         </div>
-      </main>
-
-    </div>
-  )
+    )
 }
 
-
-const query = process.env.NODE_ENV === 'development' ? 
-  groq`*[_type == "post"] | order(publishedAt desc)` : //development
-  groq`*[_type == "post" && dateTime(publishedAt) < dateTime(now())] | order(publishedAt desc)` //production
-
+const query = groq`*[_type == "author" && slug.current == "nicholas-osaka"][0]`
 
 export async function getStaticProps() {
-  const posts = await sanity.fetch(query)
+  const me = await sanity.fetch(query)
   return {
     props: {
-      posts: posts
+      me: me
     },
-    revalidate: 60 * 60 //revalidate every hour at most
+    revalidate: 3600 //revalidate page contents upon request and at most every hour
   } 
 }
 
-export default Home
+export default About
