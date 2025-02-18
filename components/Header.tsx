a2dfd3a819c042ef95fcca603a9be4609673c188
Nicholas Osaka <nicholas.osaka@gmail.com>
2025-02-18 15:08:58 -0500

    Add fancy change for link animations

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,27 +1,29 @@
 import Link from 'next/link'
 
 const Header = () => {
+    // const linkStyle = "link-animation hover:underline transition-all duration-700 hover:-translate-y-1"
+    const linkStyle = "hover:underline"
     return <>
-        <div className="px-10 md:mx-auto md:w-8/12 w-4/5 lg:flex lg:flex-row items-center py-5 sm:py-10">
+        <div className="px-10 md:mx-auto md:w-8/12 w-4/5 lg:flex lg:flex-row items-center pt-5 pb-4 lg:py-10">
             <h1 className="text-3xl font-extrabold uppercase text-left">
-                <Link href="/" className="link-animation">
+                <Link href="/" className="">
                     Nicholas Osaka
                 </Link>
             </h1>
-            <ul className="list-none mx-auto mr-0 uppercase font-bold text-xl lg:grid lg:grid-cols-2">
+            <ul className="list-none mx-auto mr-0 uppercase font-bold text-xl lg:grid lg:grid-cols-2 mt-2 lg:mt-0">
                 <li className="inline mr-5">
-                    <Link href="/writing" className="link-animation">
+                    <Link href="/writing" className={linkStyle}>
                         writing
                     </Link>
                 </li>
                 <li className="inline">
-                    <Link href="/" className="link-animation">
+                    <Link href="/" className={linkStyle}>
                         about
                     </Link>
                 </li>
             </ul>
         </div>
-        <hr className="mx-auto  border-2 border-chestnut"></hr>
+        <hr className="mx-auto border-2 border-chestnut"></hr>
     </>;
   }
   
--- pages/writing.tsx
+++ pages/writing.tsx
@@ -20,7 +20,7 @@ const Home = (props: any) => {
             {props.posts.map((post: any) => post.id && (
                 <li key={post._id} className="grid md:grid-cols-5 mb-5">
                     <Date date={post.date}/>
-                    <Link className="order-2 md:col-span-4 text-2xl font-bold link-animation" href={`/post/${post.id}`} >
+                    <Link className="order-2 md:col-span-4 text-2xl font-bold link-animation hover:underline transition-all duration-700 hover:-translate-y-1" href={`/post/${post.id}`} >
                         {post.title}
                     </Link>
                     <p className="order-3 -mt-1 md:col-start-2 md:col-span-4 font-light text-chestnut">{post.subtitle}</p>
