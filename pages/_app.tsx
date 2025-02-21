3ca88fe4f4d818ce9c990173c178665f7775cbe6
Nicholas Osaka <nicholas.osaka@gmail.com>
2025-02-20 19:31:35 -0500

    Add Vercel speed analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,11 +2,12 @@ import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import { Analytics } from '@vercel/analytics/react'
 import { MDXProvider } from '@mdx-js/react'
-
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
   <>
+    <SpeedInsights/>
     <MDXProvider>
       <Component {...pageProps} />
     </MDXProvider>
