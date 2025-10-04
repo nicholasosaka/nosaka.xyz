34d026e6036783ff4a7946b9b57819f31306c196
Nicholas Osaka <nicholas.osaka@gmail.com>
2025-10-04 11:54:42 -0400

    Update gallery

--- pages/gallery.tsx
+++ pages/gallery.tsx
@@ -15,6 +15,7 @@ const Vitae = () => {
 
             <Header fullhr/>
             <div className='my-8 w-11/12 mx-auto'>
+                <p className='text-center pt-8'>I take photos on digital cameras and analog 35mm film. Here are some of my photos. </p>
                 <div className="grid grid-cols-2 md:grid-cols-4 gap-4 my-16">
                     <div className="flex flex-col gap-4">
                         <Image className={imageStyle} width={4160} height={6240} src="/imgs/gallery/DSCF3944.JPG" alt=""/>
@@ -28,13 +29,13 @@ const Vitae = () => {
                     </div>
                     <div className="flex flex-col gap-4">
                         <img className={imageStyle} src="/imgs/gallery/DSCF2168.JPG" alt=""/>
-                        <img className={imageStyle} src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-7.jpg" alt=""/>
-                        <img className={imageStyle} src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-8.jpg" alt=""/>
+                        <img className={imageStyle} src="/imgs/gallery/DSCF7356.jpeg" alt=""/>
+                        <img className={imageStyle} src="/imgs/gallery/DSCF4693.jpg" alt=""/>
                     </div>
                     <div className="flex flex-col gap-4">
+                        <img className={imageStyle} src="/imgs/gallery/DSCF5685.jpg" alt=""/>
+                        <img className={imageStyle} src="/imgs/gallery/aperol-spritz-small.jpg" alt=""/>
                         <img className={imageStyle} src="/imgs/gallery/DSCF1884.JPG" alt=""/>
-                        <img className={imageStyle} src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-9.jpg" alt=""/>
-                        <img className={imageStyle} src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-10.jpg" alt=""/>
                     </div>
                 </div>
             </div>
